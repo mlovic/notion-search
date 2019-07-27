b038be1e9027a405a60469dbe67615dcc51c68d2
Marko Lovic <marko.lovic@earnin.com>
2019-07-27 15:12:17 -0700

    Consolidate both scripts into one

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#cp notion-scrape-search /usr/local/bin/
-#chmod +x /usr/local/bin/notion-scrape-search
-ln -s `pwd`/notion-scrape-search /usr/local/bin/notion-scrape-search
-
 #cp notion-search /usr/local/bin/
 #chmod +x /usr/local/bin/notion-search
 ln -s `pwd`/notion-search /usr/local/bin/notion-search
