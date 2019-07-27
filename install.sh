3190f667fd1ae858de70f7c1973e2c9b29ac3b33
Marko Lovic <marko.lovic@earnin.com>
2019-07-27 15:06:38 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#cp notion-scrape-search /usr/local/bin/
+#chmod +x /usr/local/bin/notion-scrape-search
+ln -s `pwd`/notion-scrape-search /usr/local/bin/notion-scrape-search
+
+#cp notion-search /usr/local/bin/
+#chmod +x /usr/local/bin/notion-search
+ln -s `pwd`/notion-search /usr/local/bin/notion-search
